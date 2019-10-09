df4bcb2e3bde72d44f47ad677e69949de42501d4
Brolyus <desousanthony@gmail.com>
2019-10-09 10:54:25 +0200

    edit on contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ function Contact(props) {
     return (
         <div className="Contact">
             <img className="avatar" src={props.image} alt={props.name} />            
-            <h4 className='name'>{props.name}</h4>
             <div className='status'>
-                <div className={props.online ? "status-online" : "status-offline"}></div>
-                <p className='status-text'> {props.online ? "Online" : "Offline" } </p>            
+                <h4 className='name'>{props.name}</h4>
+                <div className={props.online ? "status-online" : "status-offline"}/>
+                <p className='status-text'> {props.online ? "Online" : "Offline" } </p>                  
             </div>
         </div>
     )
